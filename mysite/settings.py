e9500f23c1913dd8ea0700229ea230b4ccff0402
Nahyean Kim <kingnh0328@gmail.com>
2026-03-15 18:17:40 +0900

    chore: PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mysite_db",
+        "USER": "mysite_user",
+        "PASSWORD": "mysite_password",
+        "HOST": "localhost",
+        "PORT": "5433",
     }
 }
 
